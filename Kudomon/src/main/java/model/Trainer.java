b71964abda5577c7afe2899cdab1d947100b6093
Tamas Kormendi <tamas.kormendi@kcl.ac.uk>
2017-04-13 16:19:24 +0200

    Add method to get nearby kudomons

--- Kudomon/src/main/java/model/Trainer.java
+++ Kudomon/src/main/java/model/Trainer.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Trainer {
 	
 	private String name;
@@ -11,5 +13,21 @@ public class Trainer {
 		name = n;
 		positionX = x;
 		positionY = y;
+	}
+	
+	public ArrayList<Kudomon> getNearbyKudomons(){
+		ArrayList<Kudomon> nearbyList = new ArrayList<Kudomon>();
+		
+		for (Kudomon kudo : Kudomon.getKudomonList()){
+			int kudoX = kudo.getX();
+			int kudoY = kudo.getY();
+			
+			double distance = Math.sqrt(Math.pow(positionX-kudoX, 2) + Math.pow(positionY-kudoY, 2));
+			
+			if (distance <= 100){
+				nearbyList.add(kudo);
+			}
+		}
+		return nearbyList;
 	}
 }
